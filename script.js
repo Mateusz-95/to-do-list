82e491a3a004406f5370ca0426cedd8736f7e008
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-02-17 17:32:26 +0100

    showing if a task is done and adding new task

--- script.js
+++ script.js
@@ -24,8 +24,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        };
+
+        addNewTask(newTaskContent);
+
+        render();
+
+    }
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
