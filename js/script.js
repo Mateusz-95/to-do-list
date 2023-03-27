bbf1bdbc169bd776718df9cd3c008d48a5d6a3f0
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-03-27 11:14:31 +0200

    removeTask function refactoring

--- js/script.js
+++ js/script.js
@@ -1,43 +1,54 @@
 {
-    const tasks = [];
+    let tasks = [];
+    // empty array
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        })
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
         document.querySelector(".js-newTask").value = "";
     }
+    // function addNewTask takes value from newTaskContent(input), and add to array content: newTaskContent(input value). And clear input after all.
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     }
+    // Funkcja, która za pomocą metody splice usuwa zadanie z tasks(tablicy) i ponownie wywołuje funkcję render(), czyli funkcję, która dodaje tekst(li i buttons) do htmlString, a później przypisuje to do htmla.
 
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     }
+    // Funkcja, która zmienia task.done na !task.done.
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
-
+        // Łapie wszystkie przyciski o klasie js-remove.
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
             });
         });
+        // forEach, czyli pętla, która przechodzi przez wszystkie removeButtons i po kliknięciu wywołuje funkcje removeTask(index).
 
         const toggleDoneButtons = document.querySelectorAll(".js-task");
-
+        // Łapie wszystkie js-task, czyli li i buttons
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
+                // Przechodzi za pomocą metody forEach przez wszystkie toggleDoneButtons(przyciski do zmiany zadania ze zrobionego na niezrobione i odwrotnie) i dodaje im eventListenera i po kliknięciu wywołuje funkcje toggleTaskDone(), czyli funkcje, która zmienia task.done na !task.done.
             });
         });
     }
 
     const render = () => {
         let htmlString = "";
+        // empty string 
 
         for (const task of tasks) {
             htmlString += `
@@ -50,23 +61,25 @@
             <img class="list__remove--img" src="images/trash.png">
             </button>
             </li >
-
         `;
         };
+        // pętla, która przechodzi przez tasks(tablicę) i dla kazdego task tworzy htmla(li i 2 buttony)
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
+        // Przypisuje to, co stworzyło w pętli i później zostało dodane do htmlString, do htmla a dokładnie do ul.
         bindEvents();
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
+        // Sprawia, ze formularz nie zostaje wysłany. 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        // newTaskContent get value from input
 
         if (newTaskContent === "") {
             return;
         };
+        // Jeśli input jest pusty i zostanie kliknięte dodanie nowego zadania, to nic się nie stanie.
 
         addNewTask(newTaskContent);
 
